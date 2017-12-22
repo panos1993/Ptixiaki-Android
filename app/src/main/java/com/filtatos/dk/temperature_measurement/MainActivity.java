21d587eb5104fd6460dfe090b0bfcacb22386bd6
Jim Kaltzidis <filtatos@outlook.com>
2017-12-22 23:01:05 +0200

    Initial commit

---
+++ app/src/main/java/com/filtatos/dk/temperature_measurement/MainActivity.java
@@ -0,0 +1,95 @@
+package com.filtatos.dk.temperature_measurement;
+
+import android.bluetooth.BluetoothAdapter;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+    TextView tvTemperature;
+    Bluetooth bt;
+    private static StringBuilder sb = new StringBuilder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTemperature = (TextView) findViewById(R.id.tvTemperature);
+        tvTemperature.setText("kalimera");
+
+        bt = new Bluetooth(this, mHandler);
+
+        connectService();
+
+    }
+
+
+    public void connectService() {
+        try {
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter.isEnabled()) {
+                bt.start();
+                bt.connectDevice("HC-05");///device name
+                Log.d("BLUETOOTH", "Btservice started - listening");
+            } else {
+                Log.w("BLUETOOTH", "Btservice started - bluetooth is not enabled");
+            }
+        } catch (Exception e) {
+            Log.e("BLUETOOTH", "Unable to start bt ", e);
+        }
+    }
+
+    Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case Bluetooth.MESSAGE_STATE_CHANGE:
+                    Log.d(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                    break;
+                case Bluetooth.MESSAGE_WRITE:
+                    Log.d(TAG, "MESSAGE_WRITE ");
+                    break;
+                case Bluetooth.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    String strIncom = new String(readBuf, 0, msg.arg1);
+                    sb.append(strIncom);
+                    int endOfLineIndex = sb.indexOf("\r\n");                            // determine the end-of-line
+                    if (endOfLineIndex > 0) {                                            // if end-of-line,
+                        String sbprint = sb.substring(0, endOfLineIndex);               // extract string
+                        sb.delete(0, sb.length());                                      // and clear
+                        Log.d("READ_FROM_ARDUINO", sbprint);
+                        tvTemperature.setText(sbprint + " C");
+                    }
+                    break;
+                case Bluetooth.MESSAGE_DEVICE_NAME:
+                    Log.d(TAG, "MESSAGE_DEVICE_NAME " + msg);
+                    break;
+                case Bluetooth.MESSAGE_TOAST:
+                    Log.d(TAG, "MESSAGE_TOAST " + msg);
+                    break;
+            }
+            return false;
+        }
+    });
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
